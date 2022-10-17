146ea8f915bb21e995a1bd6e8274103a551efe94
Wais000 <waisrbt1@gmail.com>
2022-10-17 12:37:21 +0200

    avatar added

--- user-controller/usersController.js
+++ user-controller/usersController.js
@@ -98,7 +98,8 @@ const avatar = new userAvatarModel({
 })
 await avatar.save()
 const user= await userModel.findById(req.user.id);
-user.avatar=`http://localhost:5000/uploads/${avatar.filename}`
+// user.avatar=`http://localhost:5000/uploads/${avatar.filename}`
+user.avatar=`https://vast-oasis-87088.herokuapp.com/uploads/${avatar.filename}`
 await user.save()
 console.log(avatar);
 res.send({user})
